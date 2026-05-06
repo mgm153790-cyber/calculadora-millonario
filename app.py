7aad675d12b74b67c34a1bf8d32e7f17fc8d0da1
donatello montenegro <mgm153790@gmail.com>
2026-05-05 19:09:54 -0600

    simplify app

--- app.py
+++ app.py
@@ -1,15 +1,12 @@
 from flask import Flask
-app = Flask(__name__)
+app = Flask(_name_)
 
 @app.route("/")
 def inicio():
-	return """
-	<h1>Calculadora del Millonario</h1>
-	<p>Bienvenido Miguel!<p>
-	<p>Con 1 millon de usuarios ganas: <b>$10,000</b></p>
-	<p>Con 10 millones de usuarios ganas: <b>$100,000</b></p>
-	<p>Con 100 millones de usuarios ganas. <b>$1,000,000</b></p>
-	"""
-
-if__name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    return """
+    <h1>Calculadora del Millonario</h1>
+    <p>Bienvenido Miguel!</p>
+    <p>Con 1 millon de usuarios ganas: <b>$10,000</b></p>
+    <p>Con 10 millones ganas: <b>$100,000</b></p>
+    <p>Con 100 millones ganas: <b>$1,000,000</b></p>
+    """
