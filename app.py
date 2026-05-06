7b646da116af66b4c5e3031b34e7ff0465f3ca52
donatello montenegro <mgm153790@gmail.com>
2026-05-05 18:04:02 -0600

    Mi primera app web

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def inicio():
+	return """
+	<h1>Calculadora del Millonario</h1>
+	<p>Bienvenido Miguel!<p>
+	<p>Con 1 millon de usuarios ganas: <b>$10,000</b></p>
+	<p>Con 10 millones de usuarios ganas: <b>$100,000</b></p>
+	<p>Con 100 millones de usuarios ganas. <b>$1,000,000</b></p>
+	"""
+
+app.run()
